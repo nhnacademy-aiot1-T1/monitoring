e22fbf7578a07c895cdcfcbf23ddd5fb196de12d
ckddms6530 <ckddms6530@gmail.com>
2024-05-08 16:07:24 +0900

    add : Sector JpaRepository 추가

---
+++ src/main/java/live/aiotone/monitoring/repository/SectorRepository.java
@@ -0,0 +1,8 @@
+package live.aiotone.monitoring.repository;
+
+import live.aiotone.monitoring.repository.entity.Sector;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SectorRepository extends JpaRepository<Sector, Long> {
+
+}
