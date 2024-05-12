b86073696b083aefeb1291c752a95e29c55b86e4
ckddms6530 <ckddms6530@gmail.com>
2024-05-12 21:34:58 +0900

    refactor : SectorServiceImpl @Transactional 추가 및 조회 시 readonly 설정

--- src/main/java/live/aiotone/monitoring/service/impl/SectorServiceImpl.java
+++ src/main/java/live/aiotone/monitoring/service/impl/SectorServiceImpl.java
@@ -6,14 +6,17 @@ import live.aiotone.monitoring.domain.Sector;
 import live.aiotone.monitoring.service.SectorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class SectorServiceImpl implements SectorService {
 
   private final SectorRepository sectorRepository;
 
   @Override
+  @Transactional(readOnly = true)
   public List<Sector> readSectorList() {
     return sectorRepository.findAll();
   }
