e7101acb62c5ca89436e92a9c63c459b7ea5a3e2
ckddms6530 <ckddms6530@gmail.com>
2024-05-13 17:41:40 +0900

    test : 통합테스트 상속용 클래스 추가

--- src/test/java/live/aiotone/monitoring/base/IntegrationTest.java
+++ src/test/java/live/aiotone/monitoring/base/IntegrationTest.java
@@ -1,14 +1,38 @@
 package live.aiotone.monitoring.base;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import live.aiotone.monitoring.setup.DbCleaner;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class IntegrationTest {
 
+  @LocalServerPort
+  protected int port;
+
+  @Autowired
+  DbCleaner cleaner;
+
+  @Autowired
+  protected ObjectMapper objectMapper;
+
+  @Autowired
+  protected MockMvc mockMvc;
+
+  @AfterEach
+  void afterTest() {
+    cleaner.databaseClean();
+  }
 
 }
