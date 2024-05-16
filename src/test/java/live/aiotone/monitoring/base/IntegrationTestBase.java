3cceeac39083116da0f303c55483108b339d5f09
ckddms6530 <ckddms6530@gmail.com>
2024-05-16 09:56:58 +0900

    test : IntegrationTest를 IntegrationTestBase로 이름 변경 및 사용 목적 Javadoc 작성.

--- src/test/java/live/aiotone/monitoring/base/IntegrationTestBase.java
+++ src/test/java/live/aiotone/monitoring/base/IntegrationTestBase.java
@@ -1,7 +1,6 @@
 package live.aiotone.monitoring.base;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.List;
 import live.aiotone.monitoring.setup.DbCleaner;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -13,22 +12,22 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.servlet.MockMvc;
 
+/**
+ * 통합테스트 공통설정 클래스 통합테스트 클래스들은 이 클래스를 상속받아 사용.
+ */
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-public class IntegrationTest {
+public class IntegrationTestBase {
 
   @LocalServerPort
   protected int port;
-
-  @Autowired
-  DbCleaner cleaner;
-
   @Autowired
   protected ObjectMapper objectMapper;
-
   @Autowired
   protected MockMvc mockMvc;
+  @Autowired
+  DbCleaner cleaner;
 
   @AfterEach
   void afterTest() {
