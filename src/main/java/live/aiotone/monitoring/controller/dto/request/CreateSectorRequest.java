d120cfcc49e83104d43c6c57668fcb065c1f7b09
ckddms6530 <ckddms6530@gmail.com>
2024-05-16 09:53:30 +0900

    feat : CreateSectorRequest 클래스 sectorName 필드 유효성 검사 추가;

--- src/main/java/live/aiotone/monitoring/controller/dto/request/CreateSectorRequest.java
+++ src/main/java/live/aiotone/monitoring/controller/dto/request/CreateSectorRequest.java
@@ -2,18 +2,17 @@ package live.aiotone.monitoring.controller.dto.request;
 
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import javax.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 /**
 * Sector 생성 요청 DTO.
 */
 @Getter
-public class CreateSectorRequest {
+@RequiredArgsConstructor(onConstructor_ = @JsonCreator)
+public final class CreateSectorRequest {
 
+  @NotBlank(message = "sectorName은 비어있을 수 없습니다.")
   private final String sectorName;
-
-  @JsonCreator
-  public CreateSectorRequest(String sectorName) {
-    this.sectorName = sectorName;
-  }
 }
