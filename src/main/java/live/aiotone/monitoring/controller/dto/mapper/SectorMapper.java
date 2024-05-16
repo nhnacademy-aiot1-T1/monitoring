d44a606d9d75bbcebd6a725fb05a9533d7d7a666
ckddms6530 <ckddms6530@gmail.com>
2024-05-16 11:02:24 +0900

    docs : SectorMapper javadoc 추가

--- src/main/java/live/aiotone/monitoring/controller/dto/mapper/SectorMapper.java
+++ src/main/java/live/aiotone/monitoring/controller/dto/mapper/SectorMapper.java
@@ -5,8 +5,18 @@ import live.aiotone.monitoring.domain.Sector;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+/**
+ * Sector 엔티티관련 Dto 매핑을 담당하는 Mapper 인터페이스.
+ */
 @Mapper(componentModel = "spring")
 public interface SectorMapper {
+
+  /**
+   * Sector 엔티티를 SectorDto로 변환.
+   *
+   * @param sector Sector 엔티티
+   * @return SectorDto
+   */
   @Mapping(source = "id", target = "sectorId")
   @Mapping(source = "sectorName", target = "sectorName")
   SectorDto toDto(Sector sector);
