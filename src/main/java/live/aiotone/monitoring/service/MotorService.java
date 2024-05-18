712def149a9bc70d420849a454f1a30a22f271a2
ckddms6530 <ckddms6530@gmail.com>
2024-05-18 23:43:35 +0900

    feat : 모터 종합 정보 조회 서비스 정의 및 구현

--- src/main/java/live/aiotone/monitoring/service/MotorService.java
+++ src/main/java/live/aiotone/monitoring/service/MotorService.java
@@ -1,6 +1,7 @@
 package live.aiotone.monitoring.service;
 
 import java.util.List;
+import live.aiotone.monitoring.domain.FactoryOverview;
 import live.aiotone.monitoring.domain.Motor;
 
 /**
@@ -14,5 +15,12 @@ public interface MotorService {
    * @return 모터 리스트.
    */
   List<Motor> readMotorList();
+
+  /**
+   * 공장 내 모터 종합 정보 조회.
+   *
+   * @return FactoryOverview;
+   */
+  FactoryOverview readFactoryOverview();
 
 }
