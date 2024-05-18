a6efa720aa10fba719be4dec5420ed5c8597743c
ckddms6530 <ckddms6530@gmail.com>
2024-05-18 17:58:59 +0900

    feat : Sensor Entity 추가

---
+++ src/main/java/live/aiotone/monitoring/domain/Sensor.java
@@ -0,0 +1,44 @@
+package live.aiotone.monitoring.domain;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 센서 Entity.
+ */
+@Entity
+@Table(name = "sensor")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Builder
+public class Sensor {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @JoinColumn(name = "motor_id")
+  @ManyToOne
+  private Motor motor;
+
+  @Column(name = "sensor_name", nullable = false)
+  private String sensorName;
+
+  @Column(name = "channel_no")
+  private int channel;
+
+
+}
