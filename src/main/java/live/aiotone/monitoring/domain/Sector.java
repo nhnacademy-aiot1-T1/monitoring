088a53718a17cb4848f1db7616ab9366bad773a4
ckddms6530 <ckddms6530@gmail.com>
2024-05-21 14:30:28 +0900

    feat : Sector 에 Motor 양방향 매핑 설정

--- src/main/java/live/aiotone/monitoring/domain/Sector.java
+++ src/main/java/live/aiotone/monitoring/domain/Sector.java
@@ -1,10 +1,12 @@
 package live.aiotone.monitoring.domain;
 
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -32,6 +34,9 @@ public class Sector {
   @Column(name = "sector_name", nullable = false)
   private String sectorName;
 
+  @OneToMany(mappedBy = "sector")
+  private List<Motor> motors;
+
   /**
    * Sector 이름으로 Sector 객체를 생성하는 메서드.
    *
@@ -39,7 +44,7 @@ public class Sector {
    * @return Sector
    */
   public static Sector createByName(String sectorName) {
-    return new Sector(null, sectorName);
+    return new Sector(null, sectorName, null);
   }
 
   /**
