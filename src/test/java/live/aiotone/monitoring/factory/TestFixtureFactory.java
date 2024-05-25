37b67c77ac7de5f22ba7636b3ffb68d85db892fb
ckddms6530 <ckddms6530@gmail.com>
2024-05-25 16:07:05 +0900

    test : MotorOverview 및 SensorScore 테스트용 팩토리 메서드 추가

--- src/test/java/live/aiotone/monitoring/factory/TestFixtureFactory.java
+++ src/test/java/live/aiotone/monitoring/factory/TestFixtureFactory.java
@@ -1,9 +1,15 @@
 package live.aiotone.monitoring.factory;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import live.aiotone.monitoring.controller.dto.MotorRunningRateDto;
+import live.aiotone.monitoring.controller.dto.response.MotorOverviewResponse;
 import live.aiotone.monitoring.domain.Motor;
+import live.aiotone.monitoring.domain.MotorOverview;
 import live.aiotone.monitoring.domain.Sector;
 import live.aiotone.monitoring.domain.SectorOverView;
+import live.aiotone.monitoring.domain.Sensor;
+import live.aiotone.monitoring.domain.SensorScore;
 
 public class TestFixtureFactory {
 
@@ -29,6 +35,12 @@ public class TestFixtureFactory {
         .motorName("motorName" + motorId)
         .on(true)
         .normal(true)
+        .build();
+  }
+
+  public static Sensor createSensor() {
+    return Sensor.builder()
+        .sensorName("sensorName")
         .build();
   }
 
@@ -61,4 +73,32 @@ public class TestFixtureFactory {
         .build();
   }
 
+  public static SensorScore createSensorScore() {
+    return SensorScore.builder()
+        .time(LocalDateTime.now())
+        .score(100)
+        .build();
+  }
+
+  public static MotorRunningRateDto createMotorRunningRateDto() {
+    String timestamp = "2022-01-01T00:00:00";
+    double rate = 0.75;
+    return new MotorRunningRateDto(timestamp, rate);
+  }
+
+  public static MotorOverview createMotorOverview() {
+    return MotorOverview.builder()
+        .totalMotorCount(10)
+        .totalOnMotorCount(7)
+        .totalNormalMotorCount(8)
+        .build();
+  }
+
+  public static MotorOverviewResponse createMotorOverviewResponseFromMotorOverview(MotorOverview motorOverview) {
+    return MotorOverviewResponse.builder()
+        .totalMotorCount(motorOverview.getTotalMotorCount())
+        .totalOnMotorCount(motorOverview.getTotalOnMotorCount())
+        .totalNormalMotorCount(motorOverview.getTotalNormalMotorCount())
+        .build();
+  }
 }
