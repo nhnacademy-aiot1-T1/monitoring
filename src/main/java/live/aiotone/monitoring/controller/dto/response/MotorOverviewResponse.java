901260fde97527f6c374d1dfe1f962887c7b0bbd
ckddms6530 <ckddms6530@gmail.com>
2024-05-26 17:26:08 +0900

    fix : MotorOverviewResponse @RequiredArgsConstructor(access = AccessLevel.PRIVATE) 삭제

--- src/main/java/live/aiotone/monitoring/controller/dto/response/MotorOverviewResponse.java
+++ src/main/java/live/aiotone/monitoring/controller/dto/response/MotorOverviewResponse.java
@@ -1,15 +1,12 @@
 package live.aiotone.monitoring.controller.dto.response;
 
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 /**
  * 모터 개요 응답 DTO.
  */
 @Getter
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class MotorOverviewResponse {
 
