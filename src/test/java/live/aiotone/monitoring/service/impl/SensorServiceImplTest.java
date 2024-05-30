3d5614d6affab3f972d074a48089465b6a96707c
ckddms6530 <ckddms6530@gmail.com>
2024-05-30 20:52:22 +0900

    test : 센서 데이터 조회 단위 테스트 추가

--- src/test/java/live/aiotone/monitoring/service/impl/SensorServiceImplTest.java
+++ src/test/java/live/aiotone/monitoring/service/impl/SensorServiceImplTest.java
@@ -2,32 +2,44 @@ package live.aiotone.monitoring.service.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import live.aiotone.monitoring.base.ServiceTestBase;
 import live.aiotone.monitoring.common.exception.sensor.SensorNotFoundException;
+import live.aiotone.monitoring.domain.Motor;
+import live.aiotone.monitoring.domain.Sensor;
+import live.aiotone.monitoring.domain.SensorData;
+import live.aiotone.monitoring.domain.SensorData.Duration;
 import live.aiotone.monitoring.domain.SensorScore;
+import live.aiotone.monitoring.factory.TestFixtureFactory;
+import live.aiotone.monitoring.repository.SensorDataRepository;
 import live.aiotone.monitoring.repository.SensorRepository;
 import live.aiotone.monitoring.repository.SensorScoreRepository;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @SuppressWarnings("NonAsciiCharacters")
 class SensorServiceImplTest extends ServiceTestBase {
 
   @InjectMocks
   SensorServiceImpl sensorService;
-
   @Mock
   SensorScoreRepository sensorScoreRepository;
   @Mock
   SensorRepository sensorRepository;
+
+  @Mock
+  SensorDataRepository sensorDataRepository;
 
   @Nested
   class SensorScore_조회 {
@@ -78,4 +90,40 @@ class SensorServiceImplTest extends ServiceTestBase {
     }
 
   }
+
+  @Test
+  void 존재하지_않는_센서_데이터를_조회할_때_SensorNotFoundException_발생() {
+    // given
+    Long sensorId = 1L;
+    when(sensorRepository.findById(sensorId)).thenReturn(Optional.empty());
+
+    // when
+    // then
+    assertThatThrownBy(() -> sensorService.readSensorData(1L, sensorId, Duration.DAY))
+        .isInstanceOf(SensorNotFoundException.class);
+    verify(sensorRepository, times(1)).findById(sensorId);
+    verify(sensorDataRepository, times(0)).findAllSensorDataByDuration(anyString(), anyString(), any(Duration.class));
+  }
+
+  @Nested
+  class SensorData_조회 {
+
+
+    @Test
+    void 조회_데이터를_반환() {
+      // given
+      Sensor testSensor = TestFixtureFactory.createSensor();
+      Motor testMotor = TestFixtureFactory.createMotor();
+      ReflectionTestUtils.setField(testSensor, "motor", testMotor);
+      when(sensorRepository.findById(1L)).thenReturn(Optional.of(testSensor));
+      when(sensorDataRepository.findAllSensorDataByDuration(anyString(), anyString(), any(Duration.class))).thenReturn(
+          List.of(SensorData.builder().build()));
+      // when
+      List<SensorData> sensorData = sensorService.readSensorData(1L, 1L, Duration.DAY);
+      // then
+      assertThat(sensorData).hasSize(1);
+    }
+  }
+
+
 }
