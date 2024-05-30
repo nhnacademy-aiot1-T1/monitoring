0c78340142b9db4d6b472a05e0aedd66fa4bd8d6
ckddms6530 <ckddms6530@gmail.com>
2024-05-30 16:37:34 +0900

    feat : 센서 데이터 도메인 추가

---
+++ src/main/java/live/aiotone/monitoring/domain/SensorData.java
@@ -0,0 +1,44 @@
+package live.aiotone.monitoring.domain;
+
+import com.influxdb.annotations.Column;
+import com.influxdb.annotations.Measurement;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import lombok.Getter;
+
+/**
+ * 센서 데이터 도메인 클래스.
+ */
+@Getter
+@Measurement(name = "rawData")
+public class SensorData {
+
+  @Column(timestamp = true)
+  private Instant time;
+
+  @Column(name = "_value")
+  private double value;
+
+  /**
+   * 센서 데이터 조회를 위한 enum 클래스.
+   */
+  @Getter
+  public enum Duration {
+    DAY("3m", 1),
+    WEEK("10m", 7),
+    MONTH("1d", 30);
+
+    private final String period;
+    private final int unit;
+
+    Duration(String period, int unit) {
+      this.period = period;
+      this.unit = unit;
+    }
+
+    public Instant getInstant() {
+      return Instant.now().minus(unit, ChronoUnit.DAYS);
+    }
+  }
+
+}
