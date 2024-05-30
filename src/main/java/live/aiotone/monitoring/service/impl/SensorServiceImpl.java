a24c29f5e5f7af026deb885d6cc0f556ab5e39a0
ckddms6530 <ckddms6530@gmail.com>
2024-05-30 16:59:20 +0900

    feat : SensorService 센서 데이터 조회 메서드 정의 및 구현

--- src/main/java/live/aiotone/monitoring/service/impl/SensorServiceImpl.java
+++ src/main/java/live/aiotone/monitoring/service/impl/SensorServiceImpl.java
@@ -3,7 +3,11 @@ package live.aiotone.monitoring.service.impl;
 import java.time.LocalDateTime;
 import java.util.List;
 import live.aiotone.monitoring.common.exception.sensor.SensorNotFoundException;
+import live.aiotone.monitoring.domain.Sensor;
+import live.aiotone.monitoring.domain.SensorData;
+import live.aiotone.monitoring.domain.SensorData.Duration;
 import live.aiotone.monitoring.domain.SensorScore;
+import live.aiotone.monitoring.repository.SensorDataRepository;
 import live.aiotone.monitoring.repository.SensorRepository;
 import live.aiotone.monitoring.repository.SensorScoreRepository;
 import live.aiotone.monitoring.service.SensorService;
@@ -21,6 +25,7 @@ public class SensorServiceImpl implements SensorService {
 
   private final SensorScoreRepository sensorScoreRepository;
   private final SensorRepository sensorRepository;
+  private final SensorDataRepository sensorDataRepository;
 
   @Override
   public List<SensorScore> readSensorScores(Long motorId, Long sensorId, LocalDateTime start, LocalDateTime end) {
@@ -28,5 +33,13 @@ public class SensorServiceImpl implements SensorService {
       throw new SensorNotFoundException(sensorId);
     }
     return sensorScoreRepository.findListSensorScoreBySensorIdAndTimeBetween(sensorId, start, end);
+  }
+
+  @Override
+  public List<SensorData> readSensorData(Long deviceId, Long sensorId, Duration duration) {
+    Sensor sensor = sensorRepository.findById(sensorId).orElseThrow(() -> new SensorNotFoundException(sensorId));
+    String motorDeviceName = sensor.getMotor().getDeviceName();
+    String sensorName = sensor.getSensorName();
+    return sensorDataRepository.findAllSensorDataByDuration(motorDeviceName, sensorName, duration);
   }
 }
